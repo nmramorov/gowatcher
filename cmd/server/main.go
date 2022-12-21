53f7aedefb2c3167f917690c8309bccc6d6c5058
Nikita Mramorov <nmramorov98@gmail.com>
2022-12-21 18:26:53 +0300

    Added partial code for increment2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,83 +1,15 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 
 	"internal/metrics"
 )
 
-type MetricsHandler struct {
-	metrics *metrics.Metrics
-}
-
-var infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-func (m *MetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	infoLog.Println("Started handling metric...")
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST requests are allowed for now", http.StatusMethodNotAllowed)
-		return
-	}
-	if r.Header.Get("Content-Type") != "text/plain" {
-		http.Error(w, "Provide proper header", http.StatusForbidden)
-		return
-	}
-	infoLog.Println("Method and Headers are valid.")
-	path := r.URL.Path
-	args := strings.Split(path, "/")
-	operation := args[1]
-	if strings.Compare(operation, "update") != 0 {
-		http.Error(w, "Provide proper operation", http.StatusNotFound)
-		return
-	}
-	infoLog.Println(args)
-	if len(args) != 5 {
-		http.Error(w, "Wrong arguments in request", http.StatusNotFound)
-		return
-	}
-	var metricType, metricName, metricValue = args[2], args[3], args[4]
-	infoLog.Printf("Received metric data:\nMetric type: %s\nMetric name: %s\nMetric value: %s", metricType, metricName, metricValue)
-	switch metricType {
-	case "gauge":
-		if _, ok := m.metrics.GaugeMetrics[metricName]; !ok {
-			http.Error(w, "No such metric exists", http.StatusInternalServerError)
-			return
-		}
-		newMetricValue, err := strconv.ParseFloat(metricValue, 64)
-		if err != nil {
-			http.Error(w, "Wrong Gauge value", http.StatusInternalServerError)
-			return
-		}
-		m.metrics.GaugeMetrics[metricName] = metrics.Gauge(newMetricValue)
-		infoLog.Printf("Value %s is set to %f", metricName, newMetricValue)
-
-	case "counter":
-		if _, ok := m.metrics.CounterMetrics[metricName]; !ok {
-			http.Error(w, "No such metric exists", http.StatusInternalServerError)
-			return
-		}
-		newMetricValue, err := strconv.ParseInt(metricValue, 10, 64)
-		if err != nil {
-			http.Error(w, "Wrong Gauge value", http.StatusInternalServerError)
-			return
-		}
-		infoLog.Printf("Value %s is set to %d", metricName, newMetricValue)
-		m.metrics.CounterMetrics[metricName] = metrics.Counter(newMetricValue)
-	default:
-		http.Error(w, "Wrong metric type", http.StatusInternalServerError)
-	}
-	w.Write([]byte("Request successfully handled"))
-}
-
 func main() {
-	infoLog.Println("Initializing web server...")
-	metricsHandler := MetricsHandler{
-		metrics: metrics.NewMetrics(),
+	metrics.InfoLog.Println("Initializing web server...")
+	metricsHandler := metrics.MetricsHandler{
+		Metrics: metrics.NewMetrics(),
 	}
 
 	server := &http.Server{
@@ -85,6 +17,6 @@ func main() {
 		Handler: &metricsHandler,
 	}
 
-	infoLog.Println("Web server is ready to accept connections...")
+	metrics.InfoLog.Println("Web server is ready to accept connections...")
 	server.ListenAndServe()
 }
---
+++ internal/handlers.go
@@ -0,0 +1,75 @@
+package metrics
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type MetricsHandler struct {
+	Metrics *Metrics
+}
+
+var InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+func (m *MetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	InfoLog.Println("Started handling metric...")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed for now", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Provide proper header", http.StatusForbidden)
+		return
+	}
+	InfoLog.Println("Method and Headers are valid.")
+	path := r.URL.Path
+	args := strings.Split(path, "/")
+	operation := args[1]
+	if strings.Compare(operation, "update") != 0 {
+		http.Error(w, "Provide proper operation", http.StatusNotFound)
+		return
+	}
+	InfoLog.Println(args)
+	if len(args) != 5 {
+		http.Error(w, "Wrong arguments in request", http.StatusNotFound)
+		return
+	}
+	var metricType, metricName, metricValue = args[2], args[3], args[4]
+	InfoLog.Printf("Received metric data:\nMetric type: %s\nMetric name: %s\nMetric value: %s", metricType, metricName, metricValue)
+	switch metricType {
+	case "gauge":
+		if _, ok := m.Metrics.GaugeMetrics[metricName]; !ok {
+			http.Error(w, "Wrong key", http.StatusInternalServerError)
+			return
+		}
+		newMetricValue, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			http.Error(w, "Wrong Gauge value", http.StatusInternalServerError)
+			return
+		}
+		m.Metrics.GaugeMetrics[metricName] = Gauge(newMetricValue)
+		InfoLog.Printf("Value %s is set to %f", metricName, newMetricValue)
+
+	case "counter":
+		if _, ok := m.Metrics.CounterMetrics[metricName]; !ok {
+			http.Error(w, "Wrong key", http.StatusInternalServerError)
+			return
+		}
+		newMetricValue, err := strconv.ParseInt(metricValue, 10, 64)
+		if err != nil {
+			http.Error(w, "Wrong Counter value", http.StatusInternalServerError)
+			return
+		}
+		InfoLog.Printf("Value %s is set to %d", metricName, newMetricValue)
+		newValue := m.Metrics.CounterMetrics[metricName] + Counter(newMetricValue)
+		m.Metrics.CounterMetrics[metricName] = newValue
+	default:
+		http.Error(w, "Wrong metric type", http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(`{"status":"ok"}`))
+}
