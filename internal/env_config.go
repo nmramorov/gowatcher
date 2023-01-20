e11f022e2142c08960d5c1d1fb4d85df7e8bd789
Nikita Mramorov <nmramorov98@gmail.com>
2023-01-20 19:48:41 +0300

    Updated ENV

--- internal/env_config.go
+++ internal/env_config.go
@@ -11,6 +11,9 @@ type EnvConfig struct {
 	Address        string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 	ReportInterval string `env:"REPORT_INTERVAL" envDefault:"10s"`
 	PollInterval   string `env:"POLL_INTERVAL" envDefault:"2s"`
+	StoreInterval  int    `env:"STORE_INTERVAL" envDefault:"300"`
+	StoreFile      string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+	Restore        bool   `env:"RESTORE" envDefault:"true"`
 }
 
 func NewConfig() (*EnvConfig, error) {
--- internal/env_config_test.go
+++ internal/env_config_test.go
@@ -12,9 +12,12 @@ func TestEnvConfig(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	assert.Equal(t, testConfig.Address, `localhost:8080`)
+	assert.Equal(t, testConfig.Address, `127.0.0.1:8080`)
 	assert.Equal(t, testConfig.ReportInterval, `10s`)
 	assert.Equal(t, testConfig.PollInterval, `2s`)
+	assert.Equal(t, testConfig.StoreInterval, 300)
+	assert.Equal(t, testConfig.StoreFile, "/tmp/devops-metrics-db.json")
+	assert.Equal(t, testConfig.Restore, true)
 }
 
 func TestEnvConfigIntervalConvertion(t *testing.T) {
