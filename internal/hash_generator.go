c4af96eed0f4f5f256229cf3b85387b5e7d1dec7
Nikita Mramorov <nmramorov98@gmail.com>
2023-02-06 08:44:12 +0300

    d

--- internal/hash_generator.go
+++ internal/hash_generator.go
@@ -15,9 +15,9 @@ func (gen *HashGenerator) GenerateHash(metricType, id string, value interface{})
 	var hashString string
 	switch metricType {
 	case "gauge":
-		hashString = fmt.Sprintf("%s:gauge:%f", id, value) + gen.secretkey
+		hashString = fmt.Sprintf("%s:gauge:%f", id, value)
 	case "counter":
-		hashString = fmt.Sprintf("%s:counter:%d", id, value) + gen.secretkey
+		hashString = fmt.Sprintf("%s:counter:%d", id, value)
 	}
 	h := hmac.New(sha256.New, []byte(gen.secretkey))
 	h.Write([]byte(hashString))
