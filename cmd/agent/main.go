6d107d247b8f125a1cf76cff352738158ca9940d
Nikita Mramorov <nmramorov98@gmail.com>
2023-06-15 02:28:11 +0300

    Added additional info about build version, date and status - should be set from command line launching "go run -ldflags ..."

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/nmramorov/gowatcher/internal/client"
 )
 
+var (
+	BuildVersion string = "N/A"
+	BuildDate    string = "N/A"
+	BuildCommit  string = "N/A"
+)
+
+func printBuildInfo() {
+	fmt.Printf("Build version: %s\n", BuildVersion)
+	fmt.Printf("Build date: %s\n", BuildDate)
+	fmt.Printf("Build commit: %s\n", BuildCommit)
+}
+
 func main() {
+	printBuildInfo()
 	client := client.Client{}
 	client.Run()
 }
