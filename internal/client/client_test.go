c61653d1f7f398fe65e4637fc0848cf6ddcf1e0c
Nikita Mramorov <nmramorov98@gmail.com>
2023-07-23 12:44:40 +0300

    Added another test for client in general

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -22,3 +22,29 @@ func TestCreateRequests(t *testing.T) {
 	endpoint := "http://127.0.0.1:8080"
 	assert.IsType(t, make([]*http.Request, 0), CreateRequests(endpoint, collector.GetMetrics()))
 }
+
+func TestPushMetricsBatch(t *testing.T) {
+	collector := col.NewCollector()
+	endpoint := "http://127.0.0.1:8080"
+
+	client := &http.Client{}
+	assert.NotPanics(t, func() { PushMetricsBatch(client, endpoint, collector.GetMetrics()) })
+}
+
+
+func TestGetMetricsValues(t *testing.T) {
+	collector := col.NewCollector()
+	endpoint := "http://127.0.0.1:8080"
+
+	client := &http.Client{}
+	assert.NotPanics(t, func() { GetMetricsValues(client, endpoint, "gauge", collector.GetMetrics()) })
+}
+
+
+func TestClientRun(t *testing.T) {
+	testFoo := func() {
+		client := Client{}
+		client.Run()
+	}
+	assert.NotPanics(t, testFoo)
+}
